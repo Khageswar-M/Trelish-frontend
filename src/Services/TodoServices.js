11e41ea33ea3ea535ea19847ae11b9a23005d842
Khageswar Maharana <ok3programmer@gmail.com>
2025-09-21 19:55:28 +0530

    Update TodoServices.js

--- src/Services/TodoServices.js
+++ src/Services/TodoServices.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = import.meta.env.VITE_API_URL;
 
-const BASE_URL = import.meta.env.MODE === "development" 
-  ? import.meta.env.VITE_API_URL_DEV 
-  : import.meta.env.VITE_API_URL_PROD;
+const BASE_URL = VITE_API_URL_DEV;
 
 export default BASE_URL;
 
