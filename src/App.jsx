797408759ed92827014cb8f61155260be8bb6cab
Khageswar Maharana <ok3programmer@gmail.com>
2025-09-23 00:54:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 
 import './App.css'
 
